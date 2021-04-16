01d1233444144d697fcd32835703f8c4ff514d36
theredlefthand <noahjoezim@gmail.com>
2021-04-16 12:13:10 +0200

    changed ethikcam.csv, added mean of ethikcams an cor.test

--- random_code_tries.R
+++ random_code_tries.R
@@ -4,7 +4,22 @@ ethikcamcsv <- read.csv("data/ethikcam.csv", header = T, sep = ';')
 
 ethikcomparison <- ethikcamcsv %>%
   summarise(Linienvergleich = ethikcamcsv$Linienpost - ethikcamcsv$Linienpre,
+            DurchgezogeneLinienvergleich = ethikcamcsv$DurchgezogeneLinienpost - ethikcamcsv$DurchgezogeneLinienpre,
             Strichevergleich = ethikcamcsv$Strichepost - ethikcamcsv$Strichepre,
-            Pfeilvergleich = ethikcamcsv$Pfeilpost - ethikcamcsv$Pfeilpre,
+            Pfeilvergleich = ethikcamcsv$DurchgezogenePfeilpost - ethikcamcsv$DurchgezogenePfeilpre,
             Gestrichelterpfeilvergleich = ethikcamcsv$Gestrichelterpfeilpost - ethikcamcsv$Gestrichelterpfeilpre,
             Knotenvergleich = ethikcamcsv$Knotenpost - ethikcamcsv$Knotenpre)
+
+ethikcomparison
+
+meanethik <- ethikcomparison %>%
+  summarise_if(is.numeric, mean) %>%
+  mutate(across(where(is.numeric), round, 2))
+
+meanethik
+
+ethikcamvergleich <- rbind(ethikcomparison, meanethik)
+
+ethikcamvergleich
+
+cor.test(ethikcomparison$Linienvergleich, ethikcomparison$Knotenvergleich)
